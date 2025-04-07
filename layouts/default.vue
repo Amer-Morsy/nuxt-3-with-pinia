f88036e6e04822ad3cd40fdf1c62ddd602c7ba1a
Amer-Morsy <amermousa341@gmail.com>
2025-04-08 02:44:25 +0300

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <header>
+      <nuxt-link to="/">
+        <h1>Adventure Store</h1>
+      </nuxt-link>
+      <nuxt-link to="/cart">
+        <h1>Cart</h1>
+      </nuxt-link>
+    </header>
+
+    <div>
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
